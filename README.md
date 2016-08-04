90896919f77af66b12570e53fb40f6454c45f0f2
Michael Walker <mike@barrucadu.co.uk>
2016-08-04 12:05:51 +0100

    Implement repeating.

--- README.md
+++ README.md
@@ -7,11 +7,15 @@
 - Start the sonic-pi server (on OSX run `ruby /Applications/Sonic Pi.app/app/server/bin/sonic-pi-server.rb`)
 - Start the bridge: `ruby bridge.rb`
 
-Send events to app `cfbbdd53d88cd46a7deb` in MT1 in the channel `music`:
+Send `do` events to app `cfbbdd53d88cd46a7deb` in MT1 in the channel `music` of the form:
 
-- Event `command`: data is a string which contains one command.
-- Event `commands`: data is a string of comma-separated commands.
-- Event `full_stop`: stop synthesis.
+```json
+{ 'commands': 'comma,separated,commands'
+, 'repeat':   'forever' OR int
+}
+```
+
+The `full_stop` event stops synthesis.
 
 Commands are:
 
--- bridge.rb
+++ bridge.rb
@@ -2,27 +2,18 @@ require 'json'
 require 'pusher-client'
 require 'sonic_pi'
 
-def docmd(app, cmd)
-  bits = cmd.split
-  if bits[0] == "sleep"
-    duration = bits[1].to_f
-    puts 'Sleeping for: ' + bits[1]
-    sleep(duration)
-  else
-    puts 'Sending command: ' + cmd
-    app.run(cmd)
+def docmds(app, cmds)
+  for cmd in cmds
+    bits = cmd.split
+    if bits[0] == "sleep"
+      duration = bits[1].to_f
+      puts 'Sleeping for: ' + bits[1]
+      sleep(duration)
+    else
+      puts 'Sending command: ' + cmd
+      app.run(cmd)
+    end
   end
-end
-
-def unquote(s)
-  if s.chars.first == '"'
-    s = s[1..-1]
-  end
-  if s.chars.last == '"'
-    s = s[0..-2]
-  end
-
-  return s
 end
 
 app = SonicPi.new
@@ -33,19 +24,43 @@ socket = PusherClient::Socket.new('cfbbdd53d88cd46a7deb')
 
 socket.subscribe('music')
 
-socket['music'].bind('command') do |data|
-  docmd(app, unquote(data))
-end
+threads = []
 
-socket['music'].bind('commands') do |data|
-  for cmd in unquote(data).split(',')
-    docmd(app,cmd)
-  end
+socket['music'].bind('do') do |data|
+  threads << Thread.new {
+    data = JSON.parse(data)
+    ntimes = 1
+
+    if data.has_key?('repeat')
+      if data['repeat'] == 'forever' then
+        ntimes = -1
+      else
+        ntimes = data['repeat'].to_i
+      end
+    end
+
+    if data.has_key?('commands')
+      cmds = data['commands'].split(',')
+      if ntimes == -1
+        while true
+          docmds(app, cmds)
+        end
+      else
+        ntimes.times do
+          docmds(app, cmds)
+        end
+      end
+    end
+  }
 end
 
 socket['music'].bind('full_stop') do |data|
   puts "Stopping"
+  for tid in threads
+    Thread.kill(tid)
+  end
   app.stop()
+  threads = []
 end
 
 socket.connect
